797680e60a1fd8f571697a8a88f3acc0bcd44e88
Alexander Coppock <acoppock@gmail.com>
2025-02-12 15:55:35 -0500

    docs

--- tests/testthat/test-position-sunflower.R
+++ tests/testthat/test-position-sunflower.R
@@ -6,7 +6,7 @@ test_that("position_sunflower works", {
 
   g <-
     ggplot(dat, aes(x, y)) +
-    geom_point(position = position_sunflower(flower_width = 0.5, flower_height = 0.5)) +
+    geom_point(position = position_sunflower()) +
     coord_cartesian(xlim = c(0, 2), ylim = c(0, 2))
 
   expect_s3_class(object = g, class = "gg")
